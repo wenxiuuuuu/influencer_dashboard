035399471a415251a6d3dba3d9012509ede64b9a
pooja nag <poojasri.n@gmail.com>
2022-11-25 00:30:35 +0800

    category mapping

--- constants.py
+++ constants.py
@@ -1,5 +1,16 @@
 CATEGORY_DICT = {
-        'fashion': ['Clothing (Brand)']
+        'fashion': ['Clothing (Brand)', 'Design & fashion', 'Jewelry/watches',
+                'Sunglasses & Eyewear Store', 'Hair Salon', 'Accessories'], 
+        'Media': ['Actor', 'Digital creator', 'Musician', 'Artist', 
+                'Photography Videography', 'DJ', 'Photographer', 'Video creator'], 
+        'Health/Wellness': ['Health/beauty', 'Cycling Studio', 'Athlete', 'Vitamins/supplements', 'Medical & health'], 
+        'Food':['Restaurant'],
+        'Electronics':['Electronics'], 
+        'Education': ['School', 'Home Improvement', ],
+        'Other': ['Home Improvement', 'Coach',
+             'Community', 'Just for fun'], 
+        'Business': ['Product/service', 'Local business', 'Shopping & retail', 'Retail company'],
+        'For Good':['Nonprofit organization', 'Youth Organization', ], 
     }
 
 # all the unique category present
