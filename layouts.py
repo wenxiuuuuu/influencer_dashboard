94e636247a0f90254e67ee8273193dd206e6b640
pooja nag <poojasri.n@gmail.com>
2022-11-25 16:09:43 +0800

    temp changes

--- layouts.py
+++ layouts.py
@@ -447,7 +447,29 @@ cluster_page = html.Div(
                 ]),
                 
                 dbc.Col([html.Div(id='hover_cluster_name'),]),
-            ])
+            ]), 
+
+            dcc.Upload(
+                id='upload-data',
+                children=html.Div([
+                    'Drag and Drop or ',
+                    html.A('Select Files')
+                ]),
+                style={
+                    'width': '100%',
+                    'height': '60px',
+                    'lineHeight': '60px',
+                    'borderWidth': '1px',
+                    'borderStyle': 'dashed',
+                    'borderRadius': '5px',
+                    'textAlign': 'center',
+                    'margin': '10px'
+                },
+                # Allow multiple files to be uploaded
+                multiple=True
+            ),
+            html.Div(id='output-data-upload'),
+
             
         ]),
 
