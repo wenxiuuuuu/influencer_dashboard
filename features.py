289914e3926a210806c2e1b69899268397a029ee
Chua Zi Heng <chuaziheng98@gmail.com>
2022-11-25 19:51:09 +0800

    remove cloudpickle dep

--- features.py
+++ features.py
@@ -1,11 +1,10 @@
-import numpy as np 
+import numpy as np
 import pickle
 from urllib.request import urlopen
-import cloudpickle as cp
 
 
 # url = 'https://drive.google.com/uc?export=view&id=1Yqdz86fVk5krNXs7hju6g5RkSPS_BUll'
 
 url = 'https://drive.google.com/u/0/uc?id=1Yqdz86fVk5krNXs7hju6g5RkSPS_BUll&export=download&confirm=t&uuid=59d57eb8-9bbf-4343-a92c-e970f15839fe&at=AHV7M3e9-gyET6zhVR8uQJfdd-mT:1669367383905'
-loaded_pickle_object = pickle.load(urlopen(url)) 
+loaded_pickle_object = pickle.load(urlopen(url))
 
